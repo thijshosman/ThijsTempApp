6ba706452ca89818402b93dfb99812bcf20bb9ab
thijshosman <thijs.hosman@gmail.com>
2015-02-06 17:33:49 -0800

    prettified observer and separated lcdhardware

--- lcdObserver.py
+++ lcdObserver.py
@@ -3,6 +3,7 @@ import threading
 import time
 import math
 from observer import *
+from LCDHardware import *
 
 class LCDDisplayUpdater(threading.Thread):
     #deprecated
@@ -29,87 +30,6 @@ class LCDDisplayUpdater(threading.Thread):
 
 
 
-class LCDHardware(object):
-    def __init__(self):
-        import Adafruit_CharLCD as LCD
-        self.lcd = LCD.Adafruit_CharLCDPlate()
-        self.lcd.clear()
-        self.lcd.message('initialized')
-        self.q=False
-
-        # create some custom characters
-        self.lcd.create_char(1, [2, 3, 2, 2, 14, 30, 12, 0])
-        self.lcd.create_char(2, [0, 1, 3, 22, 28, 8, 0, 0])
-        self.lcd.create_char(3, [0, 14, 21, 23, 17, 14, 0, 0])
-        self.lcd.create_char(4, [31, 17, 10, 4, 10, 17, 31, 0])
-        self.lcd.create_char(5, [8, 12, 10, 9, 10, 12, 8, 0])
-        self.lcd.create_char(6, [2, 6, 10, 18, 10, 6, 2, 0])
-        self.lcd.create_char(7, [31, 17, 21, 21, 21, 21, 17, 31])
-
-        # Make list of button value, text, and backlight color.
-        self.buttons = ( (LCD.SELECT, 'Select', (1,1,1)),
-            (LCD.LEFT,   'Left'  , (1,0,0)),
-            (LCD.UP,     'Up'    , (0,0,1)),
-            (LCD.DOWN,   'Down'  , (0,1,0)),
-            (LCD.RIGHT,  'Right' , (1,0,1)) )
-
-        #create default color
-        self.color=[1.0,1.0,1.0]
-
-        # # Show some basic colors.
-        # lcd.set_color(1.0, 0.0, 0.0)
-        # lcd.clear()
-        # lcd.message('RED \x01')
-        # time.sleep(3.0)
-
-        # lcd.set_color(0.0, 1.0, 0.0)
-        # lcd.clear()
-        # lcd.message('GREEN \x02')
-        # time.sleep(3.0)
-
-        # lcd.set_color(0.0, 0.0, 1.0)
-        # lcd.clear()
-        # lcd.message('BLUE \x03')
-        # time.sleep(3.0)
-
-        # lcd.set_color(1.0, 1.0, 0.0)
-        # lcd.clear()
-        # lcd.message('YELLOW \x04')
-        # time.sleep(3.0)
-
-        # lcd.set_color(0.0, 1.0, 1.0)
-        # lcd.clear()
-        # lcd.message('CYAN \x05')
-        # time.sleep(3.0)
-
-        # lcd.set_color(1.0, 0.0, 1.0)
-        # lcd.clear()
-        # lcd.message('MAGENTA \x06')
-        # time.sleep(3.0)
-
-        # lcd.set_color(1.0, 1.0, 1.0)
-        # lcd.clear()
-        # lcd.message('WHITE \x07')
-        # time.sleep(3.0)
-
-    def update(self,string):
-        self.lcd.clear()
-        self.lcd.set_color(self.color[0],self.color[1],self.color[2])
-        self.lcd.message(str(string))
-
-    def isButtonPressed(self):
-
-        for button in self.buttons:
-            if self.lcd.is_pressed(button[0]):
-                #print "button %s pressed" % button[1]
-                time.sleep(0.2)
-                return button[1]
-                #else:
-                    #return False
-
-
-
-
 
 class LCDDisplay(Observer):
     '''observer that updates the display when it is notified by the subject'''
@@ -119,10 +39,9 @@ class LCDDisplay(Observer):
         self.LCDHardware = LCDHardware
         print "initialized"
 
-
-
     def notify(self,observable, *args, **kwargs):
-        self.LCDHardware.update("got %s " % args)
+        self.LCDHardware.update(*args, **kwargs)
+
 
 
 
@@ -193,11 +112,24 @@ if __name__=='__main__':
 
     # create display observer and let it listen to the lcdlistener observable
     display1 = LCDDisplay(lcdlistener1.LCDButtonObservable,lcd1)
-    lcdlistener1.broadcast('testbutton')
 
+    # see if observer works
+    # lcdlistener1.broadcast('testbutton')
 
+    # start listening for buttons
     lcdlistener1.start()
 
+    do_exit = False
+    while do_exit == False:
+        try:
+            # sleep some time
+            time.sleep(0.1)
+        except KeyboardInterrupt:
+            # Ctrl+C was hit - exit program
+            do_exit = True
+
+    # stop all running threads
+    lcdlistener1.stop()
 
 
     # lcd1.start()
